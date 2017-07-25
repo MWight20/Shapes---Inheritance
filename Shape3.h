c6b56c345c7ce590222ce0cd1a4f3bb221f40f65
MWight20 <mwight20@gmail.com>
2017-07-25 00:29:04 -0500

    Update Shape3.h

--- Shape3.h
+++ Shape3.h
@@ -1,10 +1,4 @@
-/*
- * Program: assign10
- * Programmer: Michael Wight
- * z-id: z1644029
- * Due Date: 4/20/2016
- *
- *****************************************************************************/
+
 #ifndef SHAPE3_H
 #define SHAPE3_H
 
